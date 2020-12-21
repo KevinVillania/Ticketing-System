139cea63e6f713476c9751f99c18fd6f262dcc35
KevinVillania <75327733+KevinVillania@users.noreply.github.com>
2020-12-21 23:11:30 +0800

    Major rework
    Erased userInput function, major rework on code and algorithm

--- Ticketing system/main.cpp
+++ Ticketing system/main.cpp
@@ -1,40 +1,28 @@
-/*
-================================================
+/*================================================
 BUS TICKETING SYSTEM
 BY: Kevin Holden Villania
 written in C++, with input validation and fare
-depends on distance of destination
+depends on distance of destination, grants 20% discount
+for student and senior citizen
 FIRST BRANCH
-================================================
-*/
+================================================*/
 
 #include <iostream>
 #include <vector>
 #include <algorithm>
 #include <map>
-#include <string>
 using namespace std;
 
-//Make a print function iterator, and add function iterator
-//Add computation of fare matrix and student, senior citizen discount
-//BUGS: UNABLE TO PUSH BACK ON VECTOR NAME, FARE,
-
-//included lists of destination, input validation, ind. ticket cost
-
-double discountedPrice;
-int totalPrice, location, numPeople, age;
+double discountedPrice, totalPrice = 0;
+int location, numPeople, age;
 string name;
 char twentyDiscount;
-
 vector<int> groupAge;
 vector<double> fare;
 vector<string> name1;
 
-int userInput();
-
 int main()
 {
-
     //map of fare matrix
     map<int, int> destination_map;
     destination_map[1] = 15;
@@ -52,19 +40,16 @@ int main()
 
     cout << "WELCOME TO SOLID NORTH BUS STATION" << endl;
     cout << "_________________________________" << endl << endl;
-    //getline(cin, name);
-   // cout << name;
+
     string destination[]{"Manila","Quezon City","Marikina","Standard Fare"};
     cout << "Select destination" << endl;
 
-    //cout << destination_map[location]; fare checker
     //prints destination array
     for(int i=0; i<=3; ++i){
         cout << i+1 << " =" << " Php " << destination_map[i+1] << " ===== " << destination[i] << endl;
     }
 
     // Checks if input is in the range of 1-4
-    //cin >> location;
     while(!(cin >> location) || (location < 1 || location > 4)){
 
         cout << "Number not in range: " << endl;
@@ -79,83 +64,76 @@ int main()
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
         cout << "Invalid input, number not in range" << endl << "Enter a number: ";
     }
+    cout << endl;
 
-    //Iterator to input age of people depending on the number of participant
-    for(int i=1; i<=numPeople; ++i){
-        userInput();
+    //Iterates depending on number of passenger
+    for(int i=1; i <= numPeople; ++i){
+
+        cout << "Passenger " << i << endl;
+
+        //Gets passenger name and push it back to vector 'name1'
+        cout << "Enter name of passenger: ";
+        cin.ignore(); //clears previous cin
+        getline(cin, name);
+        name1.push_back(name);
+
+
+        //Gets passenger age and push it back to vector 'groupAge'
+        //Input validation if age is within range of 1-99
+        cout << "Age: ";
+        while(!(cin>>age)||(age < 1 || age > 99)){
+
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(),'\n');
+            cout << "Please enter age: ";
+        }
+        groupAge.push_back(age);
+
+
+        //Asks user if discount is applicable for the passenger Y for Yes and N for No
+        //Input validation if user inputs Y or N
+        cout << "With discount? Y/N: ";
+        while(!(cin>>twentyDiscount)||(twentyDiscount != 'Y' && twentyDiscount!='N')){
+
+            cin.clear();
+            cin.ignore(1000,'\n');
+            cout << "Y/N? ";
+        }
+        if (twentyDiscount == 'Y'){
+            discountedPrice = destination_map[location] * 0.8;
+            fare.push_back(discountedPrice);
+        }
+        else{
+
+            discountedPrice = destination_map[location];
+            fare.push_back(discountedPrice);
+        }
+        cout << endl;
     }
 
-    cout << "Passenger details" << endl;
+    //gets total price of tickets
+    for(int i=1; i<= fare.size(); ++i){
+        totalPrice += fare[i-1];
+    }
+
+    //PRINTS PASSENGER TICKET DETAILS
+    cout << "==============================================\n";
+    cout << "                Ticket details" << endl;
+    cout << "==============================================\n\n";
     cout << "From: Main Terminal" << endl;
-    cout << "To: " << location_map[destination_map[location]] << endl;
+    cout << "To: " << location_map[destination_map[location]] << endl; //gets destination by referencing on destination hash map
+    cout << "Total ticket price: " << totalPrice << " pesos\n";
 
+    //Iterates and prints each passenger detail
+    //prints each element in vector 'name1' and 'groupAge'
+    for(int i=1; i <= numPeople; ++i){
 
-    cout << "Total ticket price: " << totalPrice << "pesos\n";
+        cout << "\nPassenger " << i << endl;
+        cout << "Name: "<< name1[i-1] << "\n";
+        cout << "Age: " << groupAge[i-1] << "\n";
+        cout << "Ticket Price: " << fare[i-1] << " pesos" << "\n";
+    }
     cout << endl;
     cout << "==============================================" << endl;
     cout << "THANK YOU FOR RIDING WITH SOLID NORTH BUS CORP" << endl;
 }
-
-int userInput(){
-
-    double discountedPrice;
-    int totalPrice, location, numPeople, age;
-    char twentyDiscount;
-    //string name;
-
-    vector<int> groupAge;
-    vector<double> fare;
-    vector<string> name1;
-
-    map<int, int> destination_map;
-    destination_map[1] = 15;
-    destination_map[2] = 20;
-    destination_map[3] = 25;
-    destination_map[4] = 10;
-
-    //map of destination
-    map<int, string> location_map;
-    location_map[15] = "Manila";
-    location_map[20] = "Quezon City";
-    location_map[25] = "Marikina";
-    location_map[10] = "Minimum Distance";
-
-
-    cout << "Enter name of passenger: ";
-    cin.ignore();
-    getline(cin, name);
-
-    name1.push_back(name);
-
-
-
-    cout << "Age: ";
-    while(!(cin>>age)||(age < 1 || age > 99)){
-
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(),'\n');
-        cout << "Please enter age: ";
-    }
-    groupAge.push_back(age);
-
-    cout << "With discount? Y/N";
-    while(!(cin>>twentyDiscount)||(twentyDiscount != 'Y' && twentyDiscount!='N')){
-
-        cin.clear();
-        cin.ignore(1000,'\n');
-        cout << "Y/N? ";
-    }
-    if (twentyDiscount == 'Y'){
-        discountedPrice= 5;
-        fare.push_back(discountedPrice);
-    }
-    else{
-
-        discountedPrice = 1;
-        fare.push_back(discountedPrice);
-    }
-
-
-}
-
-
